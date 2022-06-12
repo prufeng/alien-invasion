94b65b2c07bb5d3f378c3a4d442f18766cc242b2
prufeng <xingzhikong@126.com>
2022-06-12 13:40:39 +0800

    Add ship and move right/left

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,20 @@
+# python -m pip install pygame
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
-    pygame.display.set_caption("Alien Invasion")
-    
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("外星人入侵") #Alien Invasion
+
+    ship = Ship(ai_settings, screen)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
 run_game()
