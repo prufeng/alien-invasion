951d33fbb77299c22f6bb4eaa6dc7c5d955598be
prufeng <xingzhikong@126.com>
2022-06-12 16:28:29 +0800

    Add bullet and fire bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 # python -m pip install pygame
 import sys
 import pygame
+from pygame.sprite import Group
+
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -8,13 +10,15 @@ import game_functions as gf
 def run_game():
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("外星人入侵") #Alien Invasion
 
     ship = Ship(ai_settings, screen)
+    bullets = Group()
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
