06afb7569214d516c0a09b340338d52230aab840
prufeng <xingzhikong@126.com>
2022-06-26 11:57:01 +0800

    Move aliens

--- game_functions.py
+++ game_functions.py
@@ -69,3 +69,17 @@ def create_fleet(ai_settings, screen, ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
+
+def update_aliens(ai_settings, aliens):
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+def check_fleet_edges(ai_settings, aliens):
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+            
+def change_fleet_direction(ai_settings, aliens):
+    for alien in aliens.sprites():
+        ai_settings.fleet_direction *= -1
